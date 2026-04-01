7051ab302aa40cb06f5ab24433b964163f1cfbcf
Kobetnerguidima <jeffersondieumercinerguidimako@gmail.com>
2026-04-01 01:05:20 +0100

    Test de mise à jour automatique v3

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello DevOps - Docker CI Pipeline!"
+    return "Hello ESTM - Mon premier pipeline CI/CD fonctionne !"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
